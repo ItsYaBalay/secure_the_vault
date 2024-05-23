ca75956155d5e9e9f545bb4116064baf03b08055
ItsYaBalay <yassin1999@gmail.com>
2024-05-23 15:26:18 -0400

    initial commit

--- code.js
+++ code.js
@@ -4,11 +4,11 @@ let greeting = "You have received this message because you have been chosen to o
 // Assign 3 variables
 
 // Creating first variable calculated using addition
-const first = 5 + 5;
+const first = 5 + 5; //10
 // Creating second variable calculated using multiplication
-const second = 10 * 4;
+const second = 10 * 4; //40
 // Creating third variable calculated using subtraction
-const third = 40 - 1;
+const third = 40 - 1; //39
 
 // Concatenating the 3 code numbers onto the greeting message
 greeting = greeting + " " + first + " " + second + " " + third;
