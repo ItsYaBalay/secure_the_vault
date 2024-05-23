8ebd31cff58b259406e78fe6b18b2fb58c3587b6
ItsYaBalay <yassin1999@gmail.com>
2024-05-23 13:36:39 -0400

    initial commit

---
+++ code.js
@@ -0,0 +1,18 @@
+// Greet the user
+let greeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Assign 3 variables
+
+// Creating first variable
+const first = 5 + 5;
+// Creating second variable
+const second = 10 * 4;
+// Creating third variable
+const third = 40 - 1;
+
+// Concatenating the 3 code numbers onto the greeting message
+greeting = greeting + " " + first + " " + second + " " + third;
+
+// displaying the popup alert with the full greeting and concatenated combination codes
+alert(greeting);
+
